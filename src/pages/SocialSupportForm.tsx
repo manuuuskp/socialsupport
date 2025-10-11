059d45487774c754648c8e659562705c650852e3
manuuuskp <manojprabhuskp@gmail.com>
2025-10-11 15:22:02 +0530

    added personal info form basic

--- src/pages/SocialSupportForm.tsx
+++ src/pages/SocialSupportForm.tsx
@@ -1,5 +1,6 @@
 import ApplicationForm from "../components/ApplicationForm";
 import ProgressBar from "../components/ProgressBar";
+import PersonalInfo from "../components/socialsupportform/PersonalInfo";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 import { setCurrentStep } from "../store/socialSupportFormSlice";
 
@@ -8,12 +9,27 @@ const SocialSupportForm = () => {
     const dispatch = useAppDispatch();
     const { currentStep } = useAppSelector(state => state.form);
 
+    const renderCurrentStep = () => {
+        switch (currentStep) {
+            case 0:
+                return <PersonalInfo />
+            default:
+                return null;
+        }
+    };
+
     return (
         <ApplicationForm>
             <ProgressBar currentStep={currentStep}
                 totalSteps={steps.length}
                 steps={steps}
                 onStepClick={(step) => dispatch(setCurrentStep(step))} />
+
+            <div className="bg-white shadow-lg rounded-lg p-4 sm:p-6 md:p-8 mb-8">
+                <div className="min-h-[300px] sm:min-h-[400px]">
+                    {renderCurrentStep()}
+                </div>
+            </div>
         </ApplicationForm>
 
     )
